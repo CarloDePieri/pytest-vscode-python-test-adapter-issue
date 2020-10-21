d92bf806ee8c6e2fd727e84642cd3f2d473b5196
Carlo De Pieri <depieri.carlo@gmail.com>
2020-10-21 11:02:37 +0200

    demonstrating multiple project setup

---
+++ p1/p1t_test.py
@@ -0,0 +1,6 @@
+class TestPone:
+    """test: p1..."""
+
+    def shouldwork_p1(self):
+        """it shouldwork"""
+        assert True
---
+++ p2/p2t_test.py
@@ -0,0 +1,6 @@
+class TestPtwo:
+    """test: p1..."""
+
+    def shouldwork_too_p2(self):
+        """it shouldwork"""
+        assert True
